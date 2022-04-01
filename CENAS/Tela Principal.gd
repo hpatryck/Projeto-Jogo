d810afaaf5272094a19dcb5f37e8ffd629721ba2
mellissy <mellissylima@gmail.com>
2022-04-01 20:18:25 -0300

    coloquei o video

--- CENAS/Tela Principal.gd
+++ CENAS/Tela Principal.gd
@@ -1,16 +1,23 @@
 extends Control
 
 func _ready():
-	pass # Replace with function body.
+	pass
+	
+	
+
+
 
 func _on_TextureButton_pressed():
 	get_tree().change_scene("res://CENAS/Tela 2.tscn")
 
+
 func _on_TextureButton2_pressed():
 	get_tree().change_scene("res://CENAS/Tela 3.tscn")
 
+
 func _on_TextureButton3_pressed():
 	get_tree().change_scene("res://CENAS/Tela 4.tscn")
+
 
 func _on_TextureButton4_pressed():
 	get_tree().change_scene("res://CENAS/Tela 5.tscn")
---
+++ CENAS/video.gd
@@ -0,0 +1,11 @@
+extends Control
+
+onready var timer := $Timer as Timer
+
+func _ready():
+	timer.start()
+
+
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://CENAS/tela principal.tscn")
